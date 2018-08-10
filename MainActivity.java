80ff42f1390834b77bf3012397b55eec1d28c962
MIkhwanNugraha <41624875+MIkhwanNugraha@users.noreply.github.com>
2018-08-10 23:18:44 +0400

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,27 @@
+package com.dicoding.associate.myconstraintview;
+
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+    private LayoutInflater mInflater;
+    private ImageView imgGambarPoster;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+}
